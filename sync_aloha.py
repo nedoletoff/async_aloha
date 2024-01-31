844b191b81921ffffaa50901ef99f6ed0ba158d8
nedoletoff <nedoletoff@ya,ru>
2024-01-31 08:05:51 +0300

    алоха и алоха с опцией сообщение в первый раз отправляется вероятностью 1

--- sync_aloha.py
+++ sync_aloha.py
@@ -1,12 +1,14 @@
 import random
 import math
 import scipy.stats as stats
+import matplotlib.pyplot as plt
 
 from main import fact
 
 
 class Messages:
     def __init__(self, M, T, stock=0):
+        self.moved = False
         self.delay = []
         self.M = M
         self.T = T
@@ -32,16 +34,16 @@ class Messages:
         self.arr[t][m] = val
 
     def move(self, m: int, t: int) -> int:
-        mas = self.arr[t][m]
-        self.arr[t+1][m] += self.arr[t][m]
+        self.moved = True
+        self.arr[t + 1][m] += self.arr[t][m]
         self.set(m, t, 0)
-        return mas
 
     def inc(self, m: int, t: int):
         self.arr[t][m] += 1
 
     def dec(self, m: int, t: int):
         self.arr[t][m] -= 1
+        self.moved = False
         return self.delay.pop(0)
 
     def len(self):
@@ -51,10 +53,12 @@ class Messages:
         for i in range(self.T):
             for j in range(self.M):
                 self.arr[i][j] = stats.poisson.rvs(lam / self.M, size=1)[0]
-                #print(f'{i=} {j=} {self.arr[i][j]=}')
+                #if self.arr[i][j] > 1:
+                #    self.arr[i][j] = 1
+                # print(f'{i=} {j=} {self.arr[i][j]=}')
 
 
-def simulate(M, lam, T, p_ab=0.3):
+def simulate(M, lam, T, p_ab=0.3, optional=False):
     """
     Simulates a system with M users.
 
@@ -69,22 +73,28 @@ def simulate(M, lam, T, p_ab=0.3):
     if M <= 0:
         raise ValueError("M must be positive")
     # Initialize the system
-    n_message = Messages(M, T, stock=100)
+    n_message = Messages(M, T, stock=T * 10)
+    t = -1
     # Generate random number of arrivals for each user
     n_message.poisson(lam)
-    #print(n_message)
+    # print(n_message)
     message_num = n_message.callculate()
     n_arrived = 0
     delay = []
+    l_of_arr = []
 
     # Simulation loop
-    for t in range(T + 100):
+    while n_arrived != message_num:
+        t += 1
         busy = 0
-        l_of_arr = []
+        l_of_arr.clear()
         # Check if there are any arrivals
         for user in range(M):
             if n_message.get(user, t) > 0:
-                if random.random() < p_ab:
+                if optional and not n_message.moved:
+                    busy += 1
+                    l_of_arr.append(user)
+                elif random.random() < p_ab:
                     busy += 1
                     l_of_arr.append(user)
         # Process any arrivals that have arrived
@@ -94,16 +104,64 @@ def simulate(M, lam, T, p_ab=0.3):
             delay.append(t - d)
         # Move rest of messages
         for user in range(M):
-            n_message.move(user, t)
+            try:
+                n_message.move(user, t)
+            except:
+                print(f'{message_num}')
+                print(n_arrived)
+                print(f'{user=} {t=}')
+                break
 
     # Calculate the average delay and number of users
+    # T = t
     MD = sum(delay) / len(delay)
     MN = n_arrived / T
     print(f'{MD=}, {MN=}')
-    print(f'{n_arrived=}, {message_num=}, {T=}, {M=}, {lam=}, {p_ab=}')
+    print(f'{n_arrived=}, {message_num=}, {T=}, {t=}, {M=}, {lam=}, {p_ab=}')
     return MD, MN
 
 
+def calculate(M, x, T, p_ab=0.3, optional=False):
+    res = []
+    for i in x:
+        res.append(simulate(M, i, T, p_ab, optional))
+    md = []
+    mn = []
+    for i in res:
+        md.append(i[0])
+        mn.append(i[1])
+    return md, mn
+
+
 if __name__ == "__main__":
-    for i in range(1, 20):
-        simulate(i, 0.5, 100, 0.32)
+    x = [i / 100 for i in range(1, 100)]
+
+    plt.figure(1)
+    md, mn = calculate(10, x, 1000, 0.32, False)
+    plt.plot(x, md)
+    plt.title("ALOHA M[D]")
+    plt.xlabel("lambda")
+    plt.ylabel("M[D]")
+    plt.savefig("ALOHA_M_D.png")
+
+    plt.figure(2)
+    plt.title("ALOHA M[N]")
+    plt.plot(x, mn)
+    plt.xlabel("lambda")
+    plt.ylabel("M[N]")
+    plt.savefig("ALOHA_M_N.png")
+
+    plt.figure(3)
+    md, mn = calculate(10, x, 1000, 0.32, True)
+    plt.title("Optional ALOHA M[D]")
+    plt.plot(x, md)
+    plt.xlabel("lambda")
+    plt.ylabel("M[D]")
+    plt.savefig("Optional_ALOHA_M_D.png")
+
+    plt.figure(4)
+    plt.title("Optional ALOHA M[N]")
+    plt.plot(x, mn)
+    plt.xlabel("lambda")
+    plt.ylabel("M[N]")
+    plt.savefig("Optional_ALOHA_M_N.png")
